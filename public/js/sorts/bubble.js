065d209f2375697c016c8fd3059f50482f17d2b9
toocolors <188366890+toocolors@users.noreply.github.com>
2026-03-11 13:10:16 -0700

    Add Cocktail Sort

--- public/js/sorts/bubble.js
+++ public/js/sorts/bubble.js
@@ -4,10 +4,8 @@
 let sorted;
 
 // ************************************************************************************************
-// Functions
+// Sort Functions
 // ************************************************************************************************
-
-
 /**
  * Sorts array using bubble sort.
  */
@@ -34,11 +32,54 @@ async function bubbleSort() {
 }
 
 /**
+ * Sorts the array using Cocktail Shaker Sort
+ */
+async function cocktailSort() {
+    console.log("Starting Cocktail Shaker Sort");
+
+    sorted = false;
+    for(let i = 0; !sorted && i < arraySize; i++) {
+        // Reset sorted
+        sorted = true;
+
+        // Loop through array forwards
+        for(let j = i; j < arraySize - i; j++) {
+            // Check sortstate
+            if(!await checkSortstate()) {
+                return;
+            }
+
+            await bubbleStep(j);
+        }
+
+        // Check if array is sorted after forward loop
+        if(sorted) {
+            break;
+        }
+
+        // Loop through array backwards
+        for(let j = arraySize - i - 1; j > i; j--) {
+            // Check sortstate
+            if(!await checkSortstate()) {
+                return;
+            }
+
+            await cocktailStep(j);
+        }
+    }
+
+    console.log("Finished Cocktail Shaker Sort");
+}
+
+// ************************************************************************************************
+// Step Functions
+// ************************************************************************************************
+/**
  * Checks if index - 1 is greater than index.
  *  If so, swaps them and updated sorted.
  * @param {Number} index An array index.
  */
-async function bubbleStep(index) {
+async function bubbleStep(index, offset = 1) {
     // Update page
     await allowUpdate();
     playAudio(array[index]);
@@ -54,5 +95,24 @@ async function bubbleStep(index) {
         
         // Swap elements
         swap(index, index - 1);
+    }
+}
+
+async function cocktailStep(index) {
+    // Update page
+    await allowUpdate();
+    playAudio(array[index]);
+
+    // Update cursor boxes
+    clearCursors();
+    setCursor(index);
+
+    // Check if elements j and j - 1 are sorted
+    if(isGreater(index, index + 1)) {
+        // Update Sorted
+        sorted = false;
+        
+        // Swap elements
+        swap(index, index + 1);
     }
 }
