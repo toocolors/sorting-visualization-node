cd8847ec45c0f15e87f9b23680b32bef023f21ec
toocolors <188366890+toocolors@users.noreply.github.com>
2026-03-11 18:48:22 -0700

    Add Selection Sort

--- public/js/sorts/selection.js
+++ public/js/sorts/selection.js
@@ -1,8 +1,9 @@
 // ************************************************************************************************
 // Script variables
 // ************************************************************************************************
-let largest;
-let smallest;
+let largest = 0;
+let smallest = 0;
+let getLargest = false;
 const sortList = [
     ["selection", selectionSort], 
     ["double-selection", doubleSelectionSort]
@@ -15,12 +16,62 @@ const sortList = [
  * Sorts the array using Double Selection Sort.
  */
 async function doubleSelectionSort() {
-    
+
 }
 
 /**
  * Sorts the array using Selection Sort.
  */
 async function selectionSort() {
+    // Outer Loop
+    for(let i = 0; i < arraySize; i++) {
+        // Inizialize Smallest
+        smallest = i;
+
+        // Inner Loop - Get Smallest Element
+        for(let j = i; j < arraySize; j++) {
+            // Check sortstate
+            if(!await checkSortstate()) {
+                return;
+            }
+
+            await selectionStep(j);
+        }
+
+        // Check sortstate
+        if(!await checkSortstate()) {
+            return;
+        }
+
+        // Swap Smallest
+        swap(i, smallest);
+
+        // Update Page
+        await allowUpdate();
+    }
+}
+
+// ************************************************************************************************
+// Step Functions
+// ************************************************************************************************
+
+/**
+ * Compares the current index to largest and smallest and updates them if necessary.
+ */
+async function selectionStep(index) {
+    // Update Page, Play Sound
+    clearClass("cursor");
+    setCursor(index);
+    await allowUpdate();
+    playAudio(array[index]);
     
+    // Update largest
+    if(getLargest && isGreater(index, largest)) {
+        largest = index;
+    }
+
+    // Update smallest
+    if(isGreater(smallest, index)) {
+        smallest = index;
+    }
 }
