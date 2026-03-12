7f72aba95fcde668bd1ba37dedad16719ef8efd9
toocolors <188366890+toocolors@users.noreply.github.com>
2026-03-11 18:14:23 -0700

    Update selection.js

---
+++ public/js/sorts/selection.js
@@ -0,0 +1,26 @@
+// ************************************************************************************************
+// Script variables
+// ************************************************************************************************
+let largest;
+let smallest;
+const sortList = [
+    ["selection", selectionSort], 
+    ["double-selection", doubleSelectionSort]
+];
+
+// ************************************************************************************************
+// Sort Functions
+// ************************************************************************************************
+/**
+ * Sorts the array using Double Selection Sort.
+ */
+async function doubleSelectionSort() {
+    
+}
+
+/**
+ * Sorts the array using Selection Sort.
+ */
+async function selectionSort() {
+    
+}
