5335d8abab28109f466a52f7668d981f24b22e8b
toocolors <188366890+toocolors@users.noreply.github.com>
2026-03-14 16:41:12 -0700

    Remove minWindowSize

--- public/js/globals.js
+++ public/js/globals.js
@@ -10,7 +10,6 @@ const gain = audio.createGain();
 let generated = false;
 const maxSpeed = 1000;
 const maxWindowSize = 4000;
-const minWindowSize = 10;
 let sortstate = -1; // -1 = stop/no sorting active, 0 = pause, 1 = step, 2 = play
 const oscillator = audio.createOscillator();
 let sorting = false;
