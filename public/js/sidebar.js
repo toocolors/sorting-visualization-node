a81ce8982411587c904ab4377d3f80626fd165db
toocolors <188366890+toocolors@users.noreply.github.com>
2026-03-25 03:17:38 -0700

    Update sidebar.js

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -4,6 +4,7 @@
 document.getElementById("menuToggle").addEventListener("click", () => {
     // Show Sidebar
     document.querySelector("nav").classList.toggle("open");
+    document.getElementById("container").classList.toggle("sidebarOpen");
     document.getElementById("overlay").classList.toggle("show");
 
     // Update Hamburger Menu Text
@@ -18,6 +19,7 @@ document.getElementById("menuToggle").addEventListener("click", () => {
 document.getElementById("overlay").addEventListener("click", () => {
     // Hide Sidebar
     document.querySelector("nav").classList.remove("open");
+    document.getElementById("container").classList.remove("sidebarOpen");
     document.getElementById("overlay").classList.remove("show");
 
     // Update Hamburger Menu Text
