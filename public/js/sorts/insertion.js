f89c3841274eccd6d759adb2cd0dd8c19d2b478f
toocolors <188366890+toocolors@users.noreply.github.com>
2026-04-02 15:28:53 -0700

    Main insertion sort function returns true at end

--- public/js/sorts/insertion.js
+++ public/js/sorts/insertion.js
@@ -52,6 +52,8 @@ export async function insertionSort(start, end) {
         // End Step
         clearCursor(i);
     }
+
+    return true;
 }
 
 // ************************************************************************************************
