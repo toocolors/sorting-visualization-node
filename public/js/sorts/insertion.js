eabed118756b223cc06d46c57e7ae811a8fcaec7
toocolors <188366890+toocolors@users.noreply.github.com>
2026-04-02 15:58:07 -0700

    Add binary insertion

--- public/js/sorts/insertion.js
+++ public/js/sorts/insertion.js
@@ -40,6 +40,9 @@ export async function insertionSort(start, end) {
         // Try to insert element i
         switch(insertionType) {
             case "binary":
+                if(!await binaryInsert(i, start)) {
+                    return false;
+                }
                 break;
             case "insertion":
             default:
@@ -59,6 +62,85 @@ export async function insertionSort(start, end) {
 // ************************************************************************************************
 // Insert Functions
 // ************************************************************************************************
+
+/**
+ * Inserts the element at index using binary insertion.
+ * @param {Number} index The index of the element to insert.
+ * @param {Number} start The start of the array portion that is being sorted.
+ * @returns 
+ */
+async function binaryInsert(index, start) {
+    // Check if element is already in place
+    if (isEqualOrGreater(index, index - 1)) {
+        return true;
+    }
+    
+    // Get position to insert element at
+    let position = await binarySearch(index, start);
+    
+    // Return early if stoping sort
+    if (position === false) {
+        return false;
+    }
+
+    // Insert element at position
+    let value = get(index);
+    for(let i = index; i > position; i--) {
+        // Start Step
+        if(!await startStep(i)) {
+            return false;
+        }
+
+        // Set element at i to element at i - 1
+        set(i, get(i - 1));
+
+        // End Step
+        clearCursor(i);
+    }
+    // Set element at position to value
+    set(position, value);
+
+    return true;
+}
+
+/**
+ * Searches for the position to insert the element at index using binary search.
+ * @param {Number} index The index of the element to insert.
+ * @param {Number} start The start of the array portion that is being sorted.
+ * @returns 
+ */
+async function binarySearch(index, start) {
+    let end = index - 1;
+    let position = index;
+    // Binary Search
+    while (start <= end) {
+        // Get Middle
+        let mid = Math.floor((start + end) / 2);
+
+        // Begin Step
+        if(!await startStep(mid)) {
+            return false;
+        }
+
+        // Check which half position is in
+        if(isEqual(index, mid)) {
+            position = mid;
+            clearCursor(mid);
+            break;
+        } else if(isGreater(index, mid)) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+            position = mid;
+        }
+
+        // End Step
+        clearCursor(mid);
+    }
+
+    // Return position to insert at
+    return position;
+}
 
 /**
  * Inserts the element at index into the sorted part of the array.
