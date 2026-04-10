08805aa36ef54c52ac1a0be258b635351ad8879e
toocolors <noa.algorithms@gmail.com>
2026-04-10 14:30:43 -0700

    Wrote uplifting sort

--- public/js/sorts/uplifting.js
+++ public/js/sorts/uplifting.js
@@ -10,8 +10,32 @@ export const sortList = [ // 0 = id, 1 = name, 2 = main function
 // ************************************************************************************************
 
 /**
- * Sorts the array using Bogo Sort.
+ * Sorts the array using Uplifting Sort,
+ *  by setting elements equal to the last element if they are smaller.
  */
-async function miracleSort() {
+async function upliftingSort() {
+    // Loop through the array
+    for(let i = 1; i < array.length; i++) {
+        // Start step
+        if(!await startStep(i)) {
+            return false;
+        }
 
+        // Overwrite current element if it is smaller than last
+        if(isLess(i, i - 1)) {
+            // Update hasDeletion
+            hasDeletion = true;
+
+            // Update element
+            set(i, get(i - 1));
+
+            // Start step
+            if(!await startStep(i)) {
+                return false;
+            }
+        }
+
+        // End Step
+        clearCursor(i);
+    }
 }
