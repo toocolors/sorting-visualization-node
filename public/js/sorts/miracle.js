7a4cda6c07a19c0905d62b5895a3e68f5dcd12eb
toocolors <noa.algorithms@gmail.com>
2026-04-10 14:03:03 -0700

    Wrote miracle sort
    Miracle sort checks if the array is sorted, waits then checks again if it is not, and repeats until the array is sorted.

--- public/js/sorts/miracle.js
+++ public/js/sorts/miracle.js
@@ -13,5 +13,19 @@ export const sortList = [ // 0 = id, 1 = name, 2 = main function
  * Sorts the array using Bogo Sort.
  */
 async function miracleSort() {
+    do {
+        // Reset Sorted
+        sorted = true;
 
+        // Check if array is sorted
+        if(!await isSorted(0, array.length - 1)) {
+            return false;
+        }
+
+        // Pause if not sorted and playing (not stepping)
+        if(!sorted && sortstate == 2) {
+            await allowUpdate(0);
+        }
+
+    } while(!sorted);
 }
