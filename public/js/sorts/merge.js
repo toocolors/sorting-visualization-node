7860edc65f545e1d3299268eaa78cb2d219bbd2e
toocolors <noa.algorithms@gmail.com>
2026-04-21 17:53:27 -0700

    Wrote naive in-place mergesort

--- public/js/sorts/merge.js
+++ public/js/sorts/merge.js
@@ -4,7 +4,8 @@
 const insertion = await import("/get/algorithm?id=Insertion");
 let insertionSize;
 export const sortList = [ // 0 = id, 1 = name, 2 = main function
-    ["merge", "Merge Sort", beginSort]
+    ["merge", "Merge Sort", beginSort],
+    ["in-place", "In-Place", beginSort]
 ];
 export const optionsList = new Object();
 
@@ -36,11 +37,101 @@ async function beginSort() {
 
     // Begin Merge Sort
     switch (options[0]) {
+        case "in-place":
+            await naiveInPlace(0, array.length - 1);
+            break;
         case "merge":
         default:
             await mergeSort(0, array.length);
             break;
     }
+}
+
+/**
+ * Sorts a section of the array using In-Place Merge Sort.
+ * @param {Number} start The start of the section to sort.
+ * @param {Number} end The end of the section to sort (including).
+ * @returns True to keep sorting, False to stop sorting.
+ */
+async function naiveInPlace(start, end) {
+    // End recursion if section is empty or one element
+    if (start >= end) {
+        return true;
+    }
+
+    // Do Insertion Sort on section if section is smaller than threshold
+    if (end - start <= insertionSize) {
+        return await insertion.insertionSort(start, end);
+    }
+
+    // Get middle
+    let middle = Math.floor((end + start) / 2);
+
+    // Run recursive functions
+    if(!await naiveInPlace(start, middle) || !await naiveInPlace(middle + 1, end)) {
+        return false;
+    }
+
+    // Initialize indices
+    let left = start;
+    let right = middle + 1;
+
+    // Set Cursors
+    setCursor(left, "red");
+    setCursor(right, "blue");
+
+    // Merge left and right sections
+    while(left <= middle && right <= end) {
+        // Increment left index if left element is not greater than right element
+        if(isEqualOrLess(left, right)) {
+            // Clear cursor
+            clearCursor(left);
+
+            // Increment left
+            left++;
+
+            // Update page
+            setCursor(left, "red");
+            if(!await startStep(-1, left)) {
+                return false;
+            }
+        }
+
+        else  {
+            // Store element
+            const temp = get(right);
+
+            // Shift indices in left section to the right
+            if(!await shift(left, middle)) {
+                return false;
+            }
+
+            // Set element into gap
+            set(left, temp);
+
+            // Clear cursors
+            clearCursor(left);
+            clearCursor(right);
+
+            // Increment indices
+            left++;
+            middle++;
+            right++;
+
+            // Update Page
+            setCursor(left, "red");
+            setCursor(right, "blue");
+            if (!await startStep(-1, right)) {
+                return false;
+            }
+        }
+    }
+
+    // Clear cursors
+    clearCursor(left);
+    clearCursor(right);
+
+    return true;
 }
 
 /**
@@ -139,4 +230,31 @@ async function mergeSort(start, end) {
 
     // Return arr
     return arr;
+}
+
+// ************************************************************************************************
+// Helper Functions
+// ************************************************************************************************
+
+/**
+ * Shifts all elements from start to end to the right.
+ * @param {Number} start The start of the section to shift.
+ * @param {Number} end The end of the section to shift (including).
+ * @returns True to continue sorting, False to stop sorting.
+ */
+async function shift(start, end) {
+    for(let i = end; i >= start; i--) {
+
+        // Set element i to element i - 1
+        set(i + 1, get(i));
+
+        // Update page
+        setCursor(i, "green");
+        if (!await startStep(-1, i)) {
+            return false;
+        }
+        clearCursor(i);
+    }
+
+    return true;
 }
