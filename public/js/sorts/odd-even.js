65786f71757f1938aaf975e3a5d8b80a5d34c6a5
toocolors <noa.algorithms@gmail.com>
2026-04-24 16:55:47 -0700

    Write synchronous Odd-Even Sort

--- public/js/sorts/odd-even.js
+++ public/js/sorts/odd-even.js
@@ -2,7 +2,8 @@
 // Script variables
 // ************************************************************************************************
 export const sortList = [ // 0 = id, 1 = name, 2 = main function
-    ["sync", "Odd-Even Sort", beginSort]
+    ["odd-even", "Odd-Even Sort", beginSort],
+    ["concurrent-odd-even", "Concurrent Odd-Even", beginSort]
 ];
 
 // ************************************************************************************************
@@ -13,5 +14,59 @@ export const sortList = [ // 0 = id, 1 = name, 2 = main function
  * Gets sorting options and begins Odd-Even Sort.
  */
 async function beginSort() {
-    
+    // Get sort options
+    const options = getSortOptions();
+
+    // Start sorting
+    await oddEvenSort(options[0] === "concurrent-odd-even" ? true : false);
+}
+
+async function oddEvenSort(concurrent) {
+    // Reset sorted
+    sorted = false;
+
+    while(!sorted) {
+        // Reset sorted
+        sorted = true;
+
+        // Run asynchronously
+
+        // Run synchronously
+        if (!await loop(0) || !await loop(1)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// ************************************************************************************************
+// Synchronous Sort Functions
+// ************************************************************************************************
+
+async function loop(start) {
+    // Loop through array
+    for(let i = start; i < array.length - 1; i += 2) {
+        // Update page
+        if (!await startStep(i)) {
+            return false;
+        }
+
+        // Swap elements if they are out of order
+        if (isGreater(i, i + 1)) {
+            // Swap element
+            swap(i, i + 1);
+            sorted = false;
+
+            // Update page
+            if(!await startStep(i)) {
+                return false;
+            }
+        }
+
+        // Clear cursor
+        clearCursor(i);
+    }    
+
+    return true;
 }
