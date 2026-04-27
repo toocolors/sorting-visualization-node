7ee09241eec209b27397d518a8ebbf53a4eddfd9
toocolors <noa.algorithms@gmail.com>
2026-04-27 16:19:27 -0700

    Added doc comments to odd-even functions

--- public/js/sorts/odd-even.js
+++ public/js/sorts/odd-even.js
@@ -9,6 +9,10 @@ export const sortList = [ // 0 = id, 1 = name, 2 = main function
 // Sort Functions
 // ************************************************************************************************
 
+/**
+ * Sorts the array using Odd-Even Sort.
+ * @returns True to continue sorting, False to stop sorting
+ */
 async function oddEvenSort() {
     // Reset sorted
     sorted = false;
@@ -26,6 +30,11 @@ async function oddEvenSort() {
     return true;
 }
 
+/**
+ * Performs one pass on the array starting from start.
+ * @param {Number} start The starting index.
+ * @returns True to continue sorting, False to stop sorting
+ */
 async function loop(start) {
     // Loop through array
     for(let i = start; i < array.length - 1; i += 2) {
