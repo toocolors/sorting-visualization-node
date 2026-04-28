c6cd274defb8d2eebf926657f43bc392e79cbb54
toocolors <noa.algorithms@gmail.com>
2026-04-27 17:08:54 -0700

    Write Weave Sort for insertion

--- public/js/sorts/weave.js
+++ public/js/sorts/weave.js
@@ -1,9 +1,20 @@
 // ************************************************************************************************
 // Script variables
 // ************************************************************************************************
+const sorts = await import("/get/algorithm?id=OffsetSorts");
 export const sortList = [ // 0 = id, 1 = name, 2 = main function
-    ["insertion", "Insertion Weave", beginSort]
+    ["insertion", "Weave (Insertion)", beginSort]
 ];
+export const optionsList = new Object();
+let threadCount;
+let sortType;
+
+// Options
+// Default
+optionsList["default"] = `
+Threads:
+<input type='number' value='2' id='threadOption'>
+`;
 
 // ************************************************************************************************
 // Sort Functions
@@ -13,5 +24,51 @@ export const sortList = [ // 0 = id, 1 = name, 2 = main function
  * Gets sort options and begins sort.
  */
 async function beginSort() {
-    
+    // Get sort options
+    const options = getSortOptions();
+    sortType = options[0];
+    threadCount = parseInt(options[1]);
+    // Clamp threadCount
+    if(isNaN(threadCount) || threadCount < 2) {
+        threadCount = 2;
+    }
+
+
+    // Begin sorting
+    await weaveSort();
+}
+
+/**
+ * Sorts the array using Weave Sort.
+ * @returns True to continue sorting, False to stop sorting
+ */
+async function weaveSort() {
+    // Create threads
+    const threads = [];
+    for(let i = 0; i < threadCount; i++) {
+        switch(sortType) {
+            case "insertion":
+            default:
+                threads.push(sorts.insertionSort(i, array.length - 1, threadCount, threadCount));
+                break;
+        }
+    }
+
+    // Await threads
+    await Promise.all(threads);
+
+    // Exit if sorting has stopped
+    if(!await checkSortstate()) {
+        return false;
+    }
+
+    // Perform final pass
+    switch(sortType) {
+        case "insertion":
+        default:
+            if(!await sorts.insertionSort(0, array.length - 1, 1, 1));
+            break;
+    }
+
+    return true;
 }
